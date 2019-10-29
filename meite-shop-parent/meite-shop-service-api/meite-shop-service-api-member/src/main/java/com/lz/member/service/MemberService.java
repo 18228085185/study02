7fcf4d21eb02fc761defd896bc5dc247c061a00f
18228085185 <57079461+18228085185@users.noreply.github.com>
2019-10-29 20:34:37 +0800

    Add files via upload

--- meite-shop-parent/meite-shop-service-api/meite-shop-service-api-member/src/main/java/com/lz/member/service/MemberService.java
+++ meite-shop-parent/meite-shop-service-api/meite-shop-service-api-member/src/main/java/com/lz/member/service/MemberService.java
@@ -1,9 +1,15 @@
 package com.lz.member.service;
 
+import com.lz.core.base.BaseResponse;
+import com.lz.member.entity.UserEntity;
 import com.lz.wei.entity.AppEntity;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
 *@description : 会员服务接口
@@ -18,6 +24,18 @@ public interface MemberService {
     @ApiOperation("会员服务调用微信服务")
     @GetMapping("/memberInvokeWeixin")
     AppEntity memberInvokeWeixin();
+
+    /**
+     * 根据手机号码查询是否已经存在,如果存在返回当前用户信息
+     *
+     * @param mobile
+     * @return
+     */
+    @ApiOperation(value = "根据手机号码查询是否已经存在")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = "mobile", dataType = "String", required = true, value = "用户手机号码"), })
+    @PostMapping("/existMobile")
+    BaseResponse<UserEntity> existMobile(@RequestParam("mobile") String mobile);
 
 
 }
